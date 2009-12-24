a003894edb949e4594baa90f9ba1b07b3199df61
rberenguel <rberenguel@43ede340-e008-11de-9741-e90c74c3d541>
2009-12-24 14:13:25 +0000

    Initial import

---
+++ JS/Relateds.js
@@ -0,0 +1,196 @@
+
+Cuina=new Array();
+Cuina[0]=new Array();
+Cuina[1]=new Array();
+j=0;
+Cuina[0][j]="http://berenguel.blogspot.com/2009/12/snickerdoodles.html";
+Cuina[1][j++]="Snickerdoodles (tea time nutmeg cookies)";
+Cuina[0][j]="http://berenguel.blogspot.com/2009/11/lime-pie-recipe.html";
+Cuina[1][j++]="Key lime pie (vegan friendly)";
+Cuina[0][j]="http://berenguel.blogspot.com/2009/04/pasteli.html";
+Cuina[1][j++]="Pasteli (ancient greek recipe)";
+Cuina[0][j]="http://berenguel.blogspot.com/2009/02/curry-cheese-bread.html";
+Cuina[1][j++]="Curry and cheese bread";
+Cuina[0][j]="http://berenguel.blogspot.com/2008/09/my-grandpas-doughnuts.html";
+Cuina[1][j++]="My grandpa's doughnuts";
+Cuina[0][j]="http://berenguel.blogspot.com/2008/10/microwave-potato-chips-recipe.html";
+Cuina[1][j++]="Microwave potato chips";
+//Cuina[0][j]="";
+//Cuina[1][j++]="";
+
+Linux=new Array();
+Linux[0]=new Array();
+Linux[1]=new Array();
+j=0;
+Linux[0][j]="http://berenguel.blogspot.com/2009/09/whistle-control-your-computer.html";
+Linux[1][j++]="Whistle control your computer (Linux&amp;Mac)";
+Linux[0][j]="http://berenguel.blogspot.com/2009/08/two-weeks-still-loving-fluxbox.html";
+Linux[1][j++]="Two weeks, still loving Fluxbox";
+Linux[0][j]="http://berenguel.blogspot.com/2009/08/fluxbox-backgrounds.html";
+Linux[1][j++]="Fluxbox backgrounds";
+Linux[0][j]="http://berenguel.blogspot.com/2009/07/three-dee.html";
+Linux[1][j++]="Three dee (3-dimensional file system browsers review)";
+Linux[0][j]="http://berenguel.blogspot.com/2009/07/acer-aspire-one-89-ubuntu-fluxbox.html";
+Linux[1][j++]="Acer Aspire One 8.9' + Ubuntu + Fluxbox";
+Linux[0][j]="http://berenguel.blogspot.com/2009/11/mg-zile-quick-emacs-in-command-line.html";
+Linux[1][j++]="mg & Zile: quick emacs in the command line";
+
+// $MAC
+
+Mac=new Array();
+Mac[0]=new Array();
+Mac[1]=new Array();
+j=0;
+Mac[0][j]="http://berenguel.blogspot.com/2009/09/whistle-control-your-computer.html";
+Mac[1][j++]="Whistle control your computer (Linux&amp;Mac) Mac";
+Mac[0][j]="http://berenguel.blogspot.com/2009/09/scribuslatex-in-mac-os.html";
+Mac[1][j++]="Scribus+Latex in Mac OS";
+Mac[0][j]="http://berenguel.blogspot.com/2009/05/extremely-useful-mostly-free-mac.html";
+Mac[1][j++]="Extremely useful (mostly free) Mac applications";
+Mac[0][j]="http://berenguel.blogspot.com/2009/05/productivity-enhancers-or-time-wasting.html";
+Mac[1][j++]="Productivity enhancers (or time wasting avoiders)";
+Mac[0][j]="http://berenguel.blogspot.com/2009/05/local-proxy.html";
+Mac[1][j++]="How to get a local proxy";
+// Mac[0][j]="";
+// Mac[1][j++]="";
+// Mac[0][j]="";
+// Mac[1][j++]="";
+// Mac[0][j]="";
+// Mac[1][j++]="";
+// Mac[0][j]="";
+// Mac[1][j++]="";
+// Mac[0][j]="";
+// Mac[1][j++]="";
+// Mac[0][j]="";
+// Mac[1][j++]="";
+// Mac[0][j]="";
+// Mac[1][j++]="";
+// Mac[0][j]="";
+// Mac[1][j++]="";
+
+
+Programming=new Array();
+Programming[0]=new Array();
+Programming[1]=new Array();
+j=0;
+Programming[0][j]="http://berenguel.blogspot.com/2009/12/c-code-juicer.html";
+Programming[1][j++]="C code juicer: detecting copied programming assignments";
+Programming[0][j]="http://berenguel.blogspot.com/2009/09/whistle-control-your-computer.html";
+Programming[1][j++]="Whistle control your computer (Linux&amp;Mac)";
+Programming[0][j]="http://berenguel.blogspot.com/2009/09/my-backup-script-in-maclinux.html";
+Programming[1][j++]="My backup script in Mac/Linux";
+Programming[0][j]="http://berenguel.blogspot.com/2009/08/screenshot-via-command-line-in-linux.html";
+Programming[1][j++]="Screenshot via command-line in Linux";
+Programming[0][j]="http://berenguel.blogspot.com/2009/05/local-proxy.html";
+Programming[1][j++]="How to get a local proxy";
+Programming[0][j]="http://berenguel.blogspot.com/2009/12/google-project-hosting.html";
+Programming[1][j++]="Google project hosting";
+Programming[0][j]="http://berenguel.blogspot.com/2009/12/image-processing-history-lena.html";
+Programming[1][j++]="Image processing history: Lena";
+Programming[0][j]="http://berenguel.blogspot.com/2009/10/songs-in-code.html";
+Programming[1][j++]="Songs in code (twitter trend)";
+Programming[0][j]="http://berenguel.blogspot.com/2009/11/more-songsincode.html";
+Programming[1][j++]="More songs in code";
+// Programming[0][j]="";
+// Programming[1][j++]="";
+// Programming[0][j]="";
+// Programming[1][j++]="";
+// Programming[0][j]="";
+// Programming[1][j++]="";
+
+iPod=new Array();
+iPod[0]=new Array();
+iPod[1]=new Array();
+j=0;
+iPod[0][j]="http://berenguel.blogspot.com/2009/11/vector-tanks-ipod-iphone-game.html";
+iPod[1][j++]="Vector Tanks: iPod Touch / iPhone game";
+iPod[0][j]="http://berenguel.blogspot.com/2009/10/why-i-bought-ipod-touch.html";
+iPod[1][j++]="Why I bought an iPod touch (as mathematician)";
+iPod[0][j]="http://berenguel.blogspot.com/2009/11/ihold.html";
+iPod[1][j++]="iHold: an iPod touch credit card stand";
+iPod[0][j]="http://berenguel.blogspot.com/2009/11/retro-on-ipod-touch-1-mini-vmac.html";
+iPod[1][j++]="Retro on iPod Touch (1): Mini vMac";
+iPod[0][j]="http://berenguel.blogspot.com/2009/12/sbsettings-for-jailbroken-iphoneipod.html";
+iPod[1][j++]="SBSettings for jailbroken iPhone/iPod Touch";
+iPod[0][j]="http://berenguel.blogspot.com/2009/12/retro-on-ipod-touch-2-scummvm.html";
+iPod[1][j++]="Retro on iPod Touch (2): ScummVM";
+// iPod[0][j]="";
+// iPod[1][j++]="";
+// iPod[0][j]="";
+// iPod[1][j++]="";
+// iPod[0][j]="";
+// iPod[1][j++]="";
+// iPod[0][j]="";
+// iPod[1][j++]="";
+
+// Mac=new Array();
+// Mac[0]=new Array();
+// Mac[1]=new Array();
+// j=0;
+// Mac[0][j]="";
+// Mac[1][j++]="";
+// Mac[0][j]="";
+// Mac[1][j++]="";
+
+// CODE
+
+MergedList=new Array();
+
+
+function Relateds()
+{
+    var ArgumentCount;
+    var ArgumentValues;
+    LinkList = new Array();
+    LinkList[0]= new Array();
+    LinkList[1]= new Array();
+    ArgumentCount=arguments.length;
+    FullList=new Array();
+    k=0;
+    for(i=0;i<ArgumentCount;i++){
+	for(j=0;j<arguments[i][0].length;j++){
+	    Repeated=0;
+	    for(m=0;m<k;m++){
+		if(LinkList[0][m]==arguments[i][0][j]){
+		    Repeated=1;
+		    break;}
+	    }
+	    if(!Repeated){
+		LinkList[0][k]=arguments[i][0][j]; 
+		LinkList[1][k]=arguments[i][1][j];
+		k++;
+	    }
+	}
+    }
+    return LinkList;
+    //ScrambleList(MergedList);
+    
+}
+
+function randOrd(){
+    return (Math.round(Math.random())-0.5);}
+function ScrambleList(SourceList){
+    NumberList = new Array();
+    for(i=0;i<SourceList[0].length;i++){ // 
+	NumberList[i]=i;
+    }
+    NumberList.sort(randOrd);
+    CopyList = new Array();
+    CopyList[0] = new Array();
+    CopyList[1] = new Array();
+    for(i=0;i<SourceList[0].length;i++){
+	CopyList[0][i]=SourceList[0][NumberList[i]];
+	CopyList[1][i]=SourceList[1][NumberList[i]];
+    }
+    
+    return CopyList;
+    
+}
+
+function ParseList(Identifier){
+    document.write('<ul>');
+    for(i=0;i<Identifier[0].length;i++){
+	document.write('<li><a href="'+Identifier[0][i]+'">'+Identifier[1][i]+'</a></li>');
+    }
+    document.write('</ul>');
+}
