c4ed1d6c12f6ef443fff36c6931fcdef94fd42bd
rberenguel <rberenguel@43ede340-e008-11de-9741-e90c74c3d541>
2009-12-25 20:20:52 +0000

    SVN doesn't like the é in Rubén

--- trunk/Bash/ToIpod.sh
+++ trunk/Bash/ToIpod.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
+
+# Copyright 2009 Ruben Berenguel
+
+# ruben /at/ maia /dot/ ub /dot/ es
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# ToIpod: will mount DSKSOURCE/FILENAME in MOUNTPOINT, and copy all
+# files in DATASOURCEDIRECTORY to the mounted filesystem. It will set
+# all permissions, Then will unmount #and use scp to copy FILENAME to
+# your iPod/iPhone. You can download the latest version (and read the
+# GPL #license) from
+
 FILENAME=TeXFiles.dsk
 DSKSOURCE=~/Documents/
 MOUNTPOINT=/mnt/hfs-disk/
