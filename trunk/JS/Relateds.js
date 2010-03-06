c3f414a6d8e7215baa2daf151986627937570acc
RBerenguel <RBerenguel@43ede340-e008-11de-9741-e90c74c3d541>
2010-03-06 11:36:32 +0000

    Big addition, long time no adds.

--- trunk/JS/Relateds.js
+++ trunk/JS/Relateds.js
@@ -60,6 +60,20 @@ Linux[1][j++]="mg & Zile: quick emacs in the command line";
 Linux[0][j]="http://berenguel.blogspot.com/2010/01/cron-diff-wget-watch-changes-in-webpage.html";
 Linux[1][j++]="Cron, diff & wget: Watch changes in a webpage";
 
+Linux[0][j]="http://berenguel.blogspot.com/2010/01/minix-on-ipod-where-linux-started.html";
+Linux[1][j++]="Minix on the iPod: Where Linux started";
+// Linux[0][j]="";
+// Linux[1][j++]="";
+// Linux[0][j]="";
+// Linux[1][j++]="";
+// Linux[0][j]="";
+// Linux[1][j++]="";
+// Linux[0][j]="";
+// Linux[1][j++]="";
+// Linux[0][j]="";
+// Linux[1][j++]="";
+
+
 // $MAC
 
 Mac=new Array();
@@ -123,8 +137,25 @@ Programming[0][j]="http://berenguel.blogspot.com/2010/01/syncing-ipods-vmac-and-
 Programming[1][j++]="Syncing iPod's vMac and Linux: emacs on iPod Touch";
 Programming[0][j]="http://berenguel.blogspot.com/2010/01/cron-diff-wget-watch-changes-in-webpage.html";
 Programming[1][j++]="Cron, diff & wget: Watch changes in a webpage";
+Programming[0][j]="http://berenguel.blogspot.com/2010/02/cjuicer-beats-humans-and-unixs-diff.html";
+Programming[1][j++]="CJuicer beats humans (and Unix's diff)";
+Programming[0][j]="http://berenguel.blogspot.com/2010/02/debugging-with-emacsvalgrind.html";
+Programming[1][j++]="Debugging with emacs+valgrind";
+Programming[0][j]="http://berenguel.blogspot.com/2010/03/current-programming-projects.html";
+Programming[1][j++]="Current programming projects";
+Programming[0][j]="http://berenguel.blogspot.com/2010/03/forths-create-does-maybe-im-amazed.html";
+Programming[1][j++]="Forth's CREATE .. DOES> Maybe I'm amazed?";
+Programming[0][j]="http://berenguel.blogspot.com/2010/03/learning-forth-by-working-on-raytracer.html";
+Programming[1][j++]="Learning Forth by working on a Raytracer";
 // Programming[0][j]="";
 // Programming[1][j++]="";
+// Programming[0][j]="";
+// Programming[1][j++]="";
+// Programming[0][j]="";
+// Programming[1][j++]="";
+// Programming[0][j]="";
+// Programming[1][j++]="";
+
 
 // $IPOD
 
@@ -148,6 +179,24 @@ iPod[0][j]="http://berenguel.blogspot.com/2010/01/syncing-ipods-vmac-and-linux-e
 iPod[1][j++]="Syncing iPod's vMac and Linux: emacs on iPod Touch";
 iPod[0][j]="http://berenguel.blogspot.com/2010/01/free-blogging-on-go-ipodiphone.html";
 iPod[1][j++]="Free blogging on the go (iPod/iPhone)";
+iPod[0][j]="http://berenguel.blogspot.com/2010/01/minix-on-ipod-where-linux-started.html";
+iPod[1][j++]="Minix on the iPod: Where Linux started";
+iPod[0][j]="http://berenguel.blogspot.com/2010/01/touchterm-pro-ssh-for-itouch.html";
+iPod[1][j++]="TouchTerm Pro: ssh for the iTouch";
+iPod[0][j]="http://berenguel.blogspot.com/2010/01/part-of-ipad.html";
+iPod[1][j++]="Part of iPad";
+iPod[0][j]="http://berenguel.blogspot.com/2010/02/backbreaker.html";
+iPod[1][j++]="Backbreaker for iPhone/iPod review";
+iPod[0][j]="http://berenguel.blogspot.com/2010/02/all-hail-steve-jobs-and-his-ipad.html";
+iPod[1][j++]="All hail Steve Jobs and his iPad?";
+iPod[0][j]="http://berenguel.blogspot.com/2010/02/field-goal-iphoneipod-touch-game-review.html";
+iPod[1][j++]="Field Goal: iPhone/iPod Touch game review";
+iPod[0][j]="Canabalt for iPhone";
+iPod[1][j++]="http://berenguel.blogspot.com/2010/02/canabalt-for-iphone.html";
+iPod[0][j]="http://berenguel.blogspot.com/2010/02/price-vs-playability.html";
+iPod[1][j++]="Price vs playability";
+// iPod[0][j]="";
+// iPod[1][j++]="";
 // iPod[0][j]="";
 // iPod[1][j++]="";
 // iPod[0][j]="";
@@ -171,6 +220,14 @@ Games[0][j]="http://berenguel.blogspot.com/2009/11/vector-tanks-ipod-iphone-game
 Games[1][j++]="Vector Tanks: iPod Touch / iPhone game";
 Games[0][j]="http://berenguel.blogspot.com/2009/12/retro-on-ipod-touch-2-scummvm.html";
 Games[1][j++]="Retro on iPod Touch (2): ScummVM";
+Games[0][j]="http://berenguel.blogspot.com/2010/02/backbreaker.html";
+Games[1][j++]="Backbreaker for iPhone/iPod review";
+Games[0][j]="http://berenguel.blogspot.com/2010/02/field-goal-iphoneipod-touch-game-review.html";
+Games[1][j++]="Field Goal: iPhone/iPod Touch game review";
+Games[0][j]="Canabalt for iPhone";
+Games[1][j++]="http://berenguel.blogspot.com/2010/02/canabalt-for-iphone.html";
+Games[0][j]="http://berenguel.blogspot.com/2010/02/price-vs-playability.html";
+Games[1][j++]="Price vs playability";
 // Games[0][j]="";
 // Games[1][j++]="";
 // Games[0][j]="";
@@ -215,9 +272,12 @@ emacs[0][j]="http://berenguel.blogspot.com/2009/11/mg-zile-quick-emacs-in-comman
 emacs[1][j++]="mg &amp; Zile: quick emacs in the command line";
 emacs[0][j]="http://berenguel.blogspot.com/2010/01/syncing-ipods-vmac-and-linux-emacs-on.html";
 emacs[1][j++]="Syncing iPod's vMac and Linux: emacs on iPod Touch";
+emacs[0][j]="http://berenguel.blogspot.com/2010/02/debugging-with-emacsvalgrind.html";
+emacs[1][j++]="Debugging with emacs+valgrind";
 // emacs[0][j]="";
 // emacs[1][j++]="";
-
+// emacs[0][j]="";
+// emacs[1][j++]="";
 
 
 // $LATEX
@@ -291,6 +351,59 @@ Plants[0][j]="http://berenguel.blogspot.com/2010/01/lemon-seedlings-around-7-mon
 Plants[1][j++]="Lemon seedlings after 7 months";
 //Plants[0][j]="";
 //Plants[1][j++]="";
+
+Drawing=new Array();
+Drawing[0]=new Array();
+Drawing[1]=new Array();
+j=0;
+Drawing[0][j]="http://berenguel.blogspot.com/2010/02/all-hail-steve-jobs-and-his-ipad.html";
+Drawing[1][j++]="All hail Steve Jobs and his iPad";
+Drawing[0][j]="http://berenguel.blogspot.com/2010/01/fountain-pen-ink-drawing.html";
+Drawing[1][j++]="Fountain pen ink drawing";
+Drawing[0][j]="http://berenguel.blogspot.com/2009/01/bridge-at-besal-girona.html";
+Drawing[1][j++]="Bridge at Besalu";
+// Drawing[0][j]="";
+// Drawing[1][j++]="";
+// Drawing[0][j]="";
+// Drawing[1][j++]="";
+
+Forth=new Array();
+Forth[0]=new Array();
+Forth[1]=new Array();
+j=0;
+Forth[0][j]="http://berenguel.blogspot.com/2010/03/learning-forth-by-working-on-raytracer.html";
+Forth[1][j++]="Learning Forth by working on a raytracer";
+Forth[0][j]="http://berenguel.blogspot.com/2010/03/forths-create-does-maybe-im-amazed.html";
+Forth[1][j++]="Forth's CREATE .. DOES> Maybe I'm amazed?";
+// Forth[0][j]="";
+// Forth[1][j++]="";
+// Forth[0][j]="";
+// Forth[1][j++]="";
+
+Productivity=new Array();
+Productivity[0]=new Array();
+Productivity[1]=new Array();
+j=0;
+Productivity[0][j]="http://berenguel.blogspot.com/2010/03/procrastination-causes-and-cures.html";
+Productivity[1][j++]="Procrastination: Causes and cures";
+Productivity[0][j]="http://berenguel.blogspot.com/2010/01/wheriz-where-are-you-in-web-20.html";
+Productivity[1][j++]="Wheriz: where are you in Web2.0";
+Productivity[0][j]="http://berenguel.blogspot.com/2009/09/whistle-control-your-computer.html";
+Productivity[1][j++]="Whistle control your computer";
+Productivity[0][j]="http://berenguel.blogspot.com/2009/09/quick-latex-ing-with-emacs.html";
+Productivity[1][j++]="Quick LaTeX-ing with emacs";
+Productivity[0][j]="http://berenguel.blogspot.com/2009/08/two-weeks-still-loving-fluxbox.html";
+Productivity[1][j++]="Two weeks: still loving fluxbox";
+Productivity[0][j]="http://berenguel.blogspot.com/2009/05/extremely-useful-mostly-free-mac.html";
+Productivity[1][j++]="Extremely useful (mostly free) Mac applications";
+Productivity[0][j]="http://berenguel.blogspot.com/2009/05/shorthand.html";
+Productivity[1][j++]="Shorthand";
+// Productivity[0][j]="";
+// Productivity[1][j++]="";
+// Productivity[0][j]="";
+// Productivity[1][j++]="";
+// Productivity[0][j]="";
+// Productivity[1][j++]="";
 
 // Mac=new Array();
 // Mac[0]=new Array();
